4653cdca8af0b00e9a49a0f1f1b3f87f5c1ff847
Milan Bairagi <nmilan626@gmail.com>
2026-04-28 15:24:50 +0545

    fix: remove length limit from scanf for string input and improve result initialization

--- Lab 3/3.24-lab.c
+++ Lab 3/3.24-lab.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    char str1[100], str2[100], result[200] = "";
+    char str1[100], str2[100], result[200];
     printf("Enter first string: ");
-    scanf("%99s", str1);
+    scanf("%s", str1);
     printf("Enter second string: ");
-    scanf("%99s", str2);
+    scanf("%s", str2);
     
-    for (int i = 0; str1[i] != '\0'; i++)
+    int i = 0;
+    
+    for (i = 0; str1[i] != '\0'; i++)
         result[i] = str1[i];
     
-    int len = 0;
-    while (str1[len] != '\0') len++;
+    for (int j = 0; str2[j] != '\0'; i++, j++)
+        result[i] = str2[j];
     
-    for (int i = 0; str2[i] != '\0'; i++)
-        result[len + i] = str2[i];
+    result[i] = '\0';
     
-    result[len] = '\0';
     printf("Concatenated: %s\n", result);
     printf("\nProgrammed by Kshitij Nepali\n");
     return 0;
